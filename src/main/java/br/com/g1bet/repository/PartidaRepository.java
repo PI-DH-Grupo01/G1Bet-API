6e38dfdd20418142da40f2c7093919af8073a1e4
Carla Vitória <vitoriacarlafs99@gmail.com>
2023-01-14 21:06:46 -0300

    Paginação Partida

--- src/main/java/br/com/g1bet/repository/PartidaRepository.java
+++ src/main/java/br/com/g1bet/repository/PartidaRepository.java
@@ -1,14 +1,16 @@
 package br.com.g1bet.repository;
 
 import br.com.g1bet.model.Partida;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 public interface PartidaRepository extends JpaRepository<Partida, Long> {
 
-    List<Partida> findAllByTimeVisitante_Id(Long id);
-
-    List<Partida> findAllByTimeCasa_Id(Long id);
+    @Query(value = "SELECT p FROM Partida p WHERE p.timeCasa.id = ?1 OR p.timeVisitante.id = ?1")
+    Page<Partida> findAllHistoricoPorTime(Long id, Pageable pageable);
 }
